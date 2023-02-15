6cc4accc653a55acda1e25477cc8174f2781d966
Deyaa Al Deen Al Batsh <diaa82002@gmail.com>
2023-02-15 08:04:08 +0200

    v2

---
+++ js/app.js
@@ -0,0 +1,121 @@
+// var a = 'deyaa';
+
+// var x = 10;
+// var y = 7;
+// var z = x+y;
+// console.log(a,x,y,z);
+// -------------
+// var num = 85;
+
+// if(num>=90&&num<100){
+//     console.log('exilant');
+// }
+// else if(num>=80&&num<90){
+//     console.log('very good');
+// }
+// else 
+// console.log('good');
+// ------------
+// let x = 17;
+// switch(x){
+//     case 1:
+//         console.log('jan');
+//         break;
+//     case 2:
+//         console.log('feb');
+//         break;
+//     case 3:
+//         console.log('march');
+//         break;
+//     case 4:
+//         console.log('epril');
+//         break;
+//     case 5:
+//         console.log('may');
+//         break;
+//     case 6:
+//         console.log('june');
+//         break;
+//     case 7:
+//         console.log('joly');
+//         break;
+//     case 8:
+//         console.log('august');
+//         break;
+//     case 9:
+//         console.log('sep');
+//         break;
+//     case 10:
+//         console.log('oct');
+//         break;
+//     case 11:
+//         console.log('novem');
+//         break;
+//     case 12:
+//         console.log('dese');
+//         break;
+//     default:
+//         console.log('Error');
+
+// }
+// ------------
+// let arr = [50,12,24,22,22,12,10];
+// arr.push(36);
+// console.log(arr);
+// arr.pop();
+// arr.shift();
+// arr.unshift(50);
+
+// console.log(arr);
+
+// let un = new Set(arr);
+
+// console.log(un);
+
+// for(let i = 0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+// let i=0
+// while(i<arr.length){
+//     console.log(arr[i]);
+//     i++;
+// }
+// let name = "Deyaa"
+// let p = `<div>
+// <p>hello ${name}</p>
+// </div>
+// `;
+// console.log(p);
+
+// let text  = "welcome to gaza";
+// text = text.slice(5);
+// console.log(text);
+// let text  = "welcome to gaza";
+// text = text.slice(5,10);
+// console.log(text);
+// let text  = "welcome to gaza";
+// text = text.slice(-5);
+// text =text.split(' ');
+// text = text.join(' ');
+// console.log(text);
+
+// let num = (Math.random()*10)+1;
+// num =num.toFixed(2);
+// num = Math.floor(num);
+// console.log(num);
+
+
+
+
+
+
+// let num = [1,3,5,7,9];
+// let newNum = num.map(function(val,index){
+//     return val *index
+// });
+// console.log(num);
+// console.log(newNum);
+
+
+
+
